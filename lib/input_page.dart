ee1b8d68e8de3fd9432a741d5511e55d713f36c5
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-04-23 23:51:09 +1200

    results_page.dart added, navigator added.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator_flutter/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // 패키지 import
 import 'constants.dart';
@@ -238,11 +239,17 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColour,
-            margin: EdgeInsets.only(top: 10.0), // only top margin.
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>ResultsPage()));
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0), // only top margin.
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
